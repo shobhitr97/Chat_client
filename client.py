508f445826484b50ee1142325c1f12122a19d655
shobhitr97 <shobhitr97@gmail.com>
2016-03-07 20:02:23 +0530

    Added files via upload

---
+++ client.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import sys
+from timeout import timeout
+import socket
+import select
+@timeout(1)
+def takemes():
+	message=raw_input()
+	return message
+server_address=("0.0.0.0",3150)
+c_sock=socket.socket()
+name=raw_input("enter your name:")
+c_sock.connect(server_address)
+@timeout(1)
+def sockrecv(Socket):
+	data=Socket.recv(200)
+	return data
+	
+ch=1
+while True:
+	if ch:
+		c_sock.sendall(name+":"+"Hello")
+		ch=0
+	
+	try:
+		data=sockrecv(c_sock)
+	except:
+		data=None
+	#c_sock = socket.create_connection(server_address, timeout=20)
+	
+	#c_sock.settimeout(20.0)
+	
+	if data :
+		print data
+	#message=raw_input("->")
+	try:
+		message=takemes()
+	except:
+		message=None
+	if message:
+		if message!="exit":
+			c_sock.sendall("\n"+name+":"+message)
+		else:
+			c_sock.sendall("exit")
+			break
+	
+c_sock.close()
---
+++ server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+import socket
+import select
+server_address=("0.0.0.0",3150)
+sock=socket.socket()
+sock.bind(server_address)
+sock.listen(50)
+inputl=[sock]
+while True:
+	#sock_t=socket.socket()
+	
+	inputr,outputr,errorr=select.select(inputl,inputl,[],120.0)
+
+	for sock_t in inputr:
+		if sock_t == sock:
+			(connection,client_address)=sock_t.accept()
+			inputl.append(connection)
+			print >> sys.stderr, 'Adding new connection'
+		
+		else:
+			data=sock_t.recv(200)
+			i=0
+			if data!="exit":
+				for s in outputr:
+					try:
+						if s==sock_t:
+							continue
+						else:
+							s.sendall(data)
+						#i=i+1
+					except:
+						print >> sys.stderr,'.....'#'timed out socket'
+						#i=i+1
+			else:
+				inputl.remove(s)
+				s.close()
+     	
