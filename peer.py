bc2d409aea8c5bc0a1370d2e04f57e2a1acc0920
shobhitr97 <shobhitr97@gmail.com>
2016-04-01 18:31:10 +0530

    Added files via upload

--- peer.py
+++ peer.py
@@ -14,22 +14,22 @@ def grecv(socket):
 	message=socket.recv(200)
 	return message
 
-
-
-port=int(raw_input("enter the port to be requested:"))
-the_add=("0.0.0.0",port)
-
 name=raw_input("enter your name:")
 
 port1=int(raw_input("enter your port:"))
 my_add1=("0.0.0.0",port1)
+
+port=int(raw_input("enter the port to be requested:"))
+the_add=("0.0.0.0",port)
+
 message=""
 socklist=[]
 key=1							#we have to give the key
 string1=""
 k=""
-
-#to the predecessor
+s=()                         #the address of the successor
+s_s=()			     #the address of successor of successor
+#to the predecessor and new peers
 s_sock=socket.socket()
 s_sock.bind(my_add1)
 s_sock.listen(20)
@@ -55,31 +55,34 @@ socklist.append(connection)
 #to the successor
 r_sock=socket.socket()
 r_sock.connect(s)
-#k=r_sock.recv(200)                              #spam - not of any use
-r_sock.sendall(pickle.dumps(("to_successor","")))		#any message can be used instead of k
-#socklist.append(r_sock)
+r_sock.sendall(pickle.dumps(("to_successor","")))
+message=r_sock.recv(200)
+s_s=pickle.loads(message)
 
 
 while True:
-	try:
-		inputr,outputr,errorr=select.select(socklist,[],[],2.0)
-	except :
+
+	inputr,outputr,errorr=select.select(socklist,[],[],2.0)
+	'''except :
 		print "something went wrong"
 		socklist=inputr=[s_sock]
+'''
 	if inputr:
 		for t in inputr:
 			if t==s_sock:
 				connection,address=s_sock.accept()
 				message=grecv(connection)
+				connection.sendall(pickle.dumps(s))
 				socklist.append(connection)
 			else:
 				try:
 					message=grecv(t)
 					key_i,string1=pickle.loads(message)
 					if key_i != key:
-						print string1
-						r_sock.sendall(message)#pickle.dumps((key_i,string1)))
-				except:
+						if string1!="#":
+							print string1
+							r_sock.sendall(message)
+				
 					message=None
 					
 								
@@ -87,10 +90,25 @@ while True:
 		string1=getin()
 		#print string1
 		string1=name+":"+string1
-		r_sock.sendall(pickle.dumps((key,string1)))
+		#r_sock.sendall(pickle.dumps((key,string1)))
 	except:
 		#print "did not get input"
-		string1=None
+		string1="#"
+	
+	try:
+		if string1:
+			r_sock.sendall(pickle.dumps((key,string1)))
+	except:
+		print "peer removed"
+		#r_sock.close()
+		r_sock=socket.socket()
+		r_sock.connect(s_s)
+		socklist.append(r_sock)
+		s=s_s
+		r_sock.sendall(str(key-2))
+		message=r_sock.recv(200)
+		s_s=pickle.loads(message)
+		print "removed:"+s_s+"added:"+s
 
 
 
--- peer__.py
+++ peer__.py
@@ -2,6 +2,11 @@
 import sys,socket,select,pickle
 from timeout import timeout
 
+
+#Now i have to pass a message that can be that can recognize if the peer is active or not
+#So in the starting only I must know the address of the successor of the successor so that i can establish a connection with the next peer in case of failure of immediate successor
+
+
 @timeout(1)
 def getin():
 	m=""
@@ -24,9 +29,11 @@ message=""
 string1=""
 phrase=""
 key_i=1
-
 s_add=()
+s_s_add=()
+
 s_add=my_add
+s_s_add=s_add
 sock=socket.socket()
 sock.bind(my_add)
 sock.listen(20)
@@ -50,11 +57,9 @@ if phrase=="connect":
 
 
 while True:
-	try:
-		inputr,outputr,errorr=select.select(socklist,[],[],0.5)
-	except:
-		print "someone exited"
-		socklist=inputr=[sock]
+	#try:
+	inputr,outputr,errorr=select.select(socklist,[],[],0.5)
+
 	if inputr:
 		for obj in inputr:
 			if obj==sock:
@@ -64,8 +69,9 @@ while True:
 				if phrase=="connect":
 					add=pickle.loads(string1)
 					connection.sendall(pickle.dumps(s_add))
+					s_s_add=s_add
 					s_add=add
-					print add
+					print s_add
 					n_sock.close()
 					n_sock=socket.socket()
 					n_sock.connect(add)
@@ -73,26 +79,79 @@ while True:
 					key=key+1
 					#socklist.append(n_sock)
 				else:
+					n_sock.sendall(pickle.dumps(s_add))
 					socklist.append(connection)
 			else:
 				try:
 					message=grecv(obj)
 					key_i,string1=pickle.loads(message)
 					if key_i != key:
-						print string1
-						n_sock.sendall(message)
+						if string1!="#":
+							print string1
+							n_sock.sendall(message)
+			
+					
 				except:
-					message=None
+					message="None"
 					#print "did not receive"
 					
-					
-								
 	try:	
 		string1=getin()
 		#print string1
 		string1=name+":"+string1
-		n_sock.sendall(pickle.dumps((key,string1)))
+	
 	except:
 		#print "did not get input"
-		string1=None
+		string1="#"
+
+	#except:
+	#	print "someone exited"
+	
+
+	try:
+		if string1:
+			n_sock.sendall(pickle.dumps((key,string1)))
+	except:
+		if my_add!=s_s_add:
+			print "peer removed"
+			#n_sock.close()
+			n_sock=socket.socket()
+			n_sock.connect(s_s_add)
+			socklist.append(n_sock)
+			s_add=s_s_add
+			n_sock.sendall(str(key-2))
+			message=n_sock.recv(200)
+			s_s_add=pickle.loads(message)
+			print "removed:"+s_s_add+"added:"+s_add
+
+
+
+
+
+#in this case two consecutive peers can not exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 				
